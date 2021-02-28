3f5fbd6852501da486413c4fbd0043f65a2cf310
Grace Scheg <gracescheg@Graces-MacBook-Pro.local>
2021-02-28 13:40:48 -0800

    added changes

--- R_code/week4_first_data_analysis.R
+++ R_code/week4_first_data_analysis.R
@@ -2,13 +2,14 @@
 if (!require(TCGAbiolinks)) BiocManager::install("TCGAbiolinks")
 
 #What line of code needs to be added before using TCGAbiolinks?
+library(TCGAbiolinks)
 
 #add barcodes argument to query if you want to run on your local machine for smaller files downloaded
-#barcodes_rnaseq <- c("TCGA-BH-A0DG-01A-21R-A12P-07","TCGA-A2-A0YF-01A-21R-A109-07",
-#          "TCGA-AN-A04A-01A-21R-A034-07","TCGA-AR-A1AP-01A-11R-A12P-07",
-#              "TCGA-A2-A0T3-01A-21R-A115-07", "TCGA-E2-A154-01A-11R-A115-07" )
-#barcodes_clinic <- c("TCGA-BH-A0DG","TCGA-A2-A0YF","TCGA-AN-A04A","TCGA-AR-A1AP", "TCGA-A2-A0T3",
-#                      "TCGA-E2-A154", "TCGA-AO-A12F", "TCGA-A2-A0YM", "TCGA-BH-A0E0", "TCGA-AN-A0FL")
+barcodes_rnaseq <- c("TCGA-BH-A0DG-01A-21R-A12P-07","TCGA-A2-A0YF-01A-21R-A109-07",
+          "TCGA-AN-A04A-01A-21R-A034-07","TCGA-AR-A1AP-01A-11R-A12P-07",
+              "TCGA-A2-A0T3-01A-21R-A115-07", "TCGA-E2-A154-01A-11R-A115-07" )
+barcodes_clinic <- c("TCGA-BH-A0DG","TCGA-A2-A0YF","TCGA-AN-A04A","TCGA-AR-A1AP", "TCGA-A2-A0T3",
+                      "TCGA-E2-A154", "TCGA-AO-A12F", "TCGA-A2-A0YM", "TCGA-BH-A0E0", "TCGA-AN-A0FL")
 
 
 #######    Group 1: RNASeq     ############
@@ -36,50 +37,70 @@ if (!require(TCGAbiolinks)) BiocManager::install("TCGAbiolinks")
 # Use rsync to copy your create pngfile to local machine for viewing
 
 #######    Group 2: clinical   ###########
-# library(survival) #What do you need to do before running this line?
-# library(survminer) #What do you need to do before running this line?
-# library(arsenal) #What do you need to do before running this line?
-# clin_query <- GDCquery(project = "TCGA-BRCA", data.category="Clinical", file.type="xml")
-# GDCdownload( clin_query ) #should only need this command once. This downloads the files onto your system.
-# clinic <- GDCprepare_clinic(clin_query, clinical.info="patient")
-# names(clinic)[names(clinic) == "days_to_last_followup"] = "days_to_last_follow_up"
+library(survival) #What do you need to do before running this line? 
+#download survival package
+library(survminer) #What do you need to do before running this line? 
+#download survminer package
+#survival analysis
+library(arsenal) #What do you need to do before running this line? 
+#download arsenal package
+clin_query <- GDCquery(project = "TCGA-BRCA", data.category="Clinical", file.type="xml", barcode = barcodes_clinic)
+GDCdownload( clin_query) #should only need this command once. This downloads the files onto your system.
+clinic <- GDCprepare_clinic(clin_query, clinical.info="patient")
+names(clinic)[names(clinic) == "days_to_last_followup"] = "days_to_last_follow_up"
 
 #Add a new column to clinic called "age_category"
 #If age_at_initial_pathologic_diagnosis is < 40, define patient as "Young" in new column
 #If age_at_initial_pathologic_diagnosis is >= 60, define patient as "Old" in new column
 #Other patients (between 40 and 60), define as "Mid"
+age_clinical <- clinic$age_at_initial_pathologic_diagnosis
+clinic$age_category <- ifelse(age_clinical < 40, "Young", ifelse(age_clinical >= 60, "Old", "Mid"))
 
 #use tableone to create summary of clinic data
-# subtypes <- TCGAquery_subtype(tumor = "BRCA")
+#allows you to summarize data as counts/percentages
+library(tableone)
+clinicalsummary <- CreateTableOne(data = clinic)
+subtypes <- TCGAquery_subtype(tumor = "BRCA") #creating a new separate dataframe with all patients with BRCA tumors
 # Add the age_category column in the same way as above
+age_subtypes = subtypes$age_at_initial_pathologic_diagnosis
+subtypes$age_category <- ifelse(age_subtypes < 40, "Young", ifelse(age_subtypes >= 60, "Old", "Mid"))
+
 # How many patients are in subtypes vs clinic? Why?
+nrow(clinic)
+nrow(subtypes)
 
 # BELOW CODE WILL NOT RUN. This is an example from another dataset
 # Consider what you need to change for it to run on clinic
 # Hint: age_category stays the same. "Oncotree.Code" was the name of a column
 # Option to use either subtypes or clinic dataframe
-# table_arse <- tableby(age_category ~ (Age.at.Diagnosis) +(Oncotree.Code) +
-#          (ER.Status) + (Pam50) + (Tumor.Stage) + (Ethnicity.Call), data=clinic_oneone,
-#          numeric.test="kwt", cat.test="fe", numeric.stats = c("Nmiss", "meansd"), total=FALSE)
-# df <- as.data.frame( summary(table_arse, text=TRUE, pfootnote=TRUE) )                                                                                                                                                                                             df <- as.data.frame( summary(table_arse, text=TRUE, pfootnote=TRUE) )
-# write.csv(df, “filepath/filename.csv”, row.names=FALSE)
+names(subtypes) = make.names(names(subtypes)) #cleans up names in the subtypes
+table_arse <- tableby(age_category ~ (pathologic_stage)+ (mRNA.Clusters)+
+         (BRCA_Pathology), data=subtypes,
+         numeric.test="kwt", cat.test="chisq", numeric.stats = c("Nmiss", "meansd"), total=FALSE)
+df <- as.data.frame(summary(table_arse, text=TRUE, pfootnote=TRUE) )                                                                                                                                                                                             df <- as.data.frame( summary(table_arse, text=TRUE, pfootnote=TRUE) )
+write.csv(df,"data/tableby.csv", row.names=FALSE)
 
 # Modify below code for different variables of interest
 # The "gender" can be changed to any of the column names of the clinic dataframe
 # Look at the dataframe via the str(clinic) command
-# TCGAanalyze_survival( clinic, "gender", filename="survival_gender.pdf")
-#TCGAanalyze_survival( clinic, "age_category", filename="survival_age_category.pdf")
+str(clinic)
+#TCGAanalyze_survival() creates a survival plot from clinical data
+#uses Kaplan Meier
+TCGAanalyze_survival(clinic, "ethnicity", filename="./survival_curves/survival_ethnicity.pdf")
+TCGAanalyze_survival( clinic, "menopause_status", filename="./survival_curves/survival_menopause.pdf")
+TCGAanalyze_survival(clinic, "breast_carcinoma_progesterone_receptor_status", filename="./survival_curves/survival_progesterone.pdf")
 # use ls to confirm the file was created
 # rsync to copy and view on local computer
 
-# overall_survival <- as.integer( ifelse( is.na(clinic$days_to_death), clinic$days_to_last_follow_up, clinic$days_to_death) )
-# clinic$overall_survival <- overall_survival
-# clinic$death_event <- ifelse(clinic$patient.vital_status == 'alive', 0,1)
-# colnames(clinic) #use if want to visually see affect of ^^
-# cox_fit <- coxph(Surv(overall_survival, death_event)~age_at_initial_pathologic_diagnosis, data=clinic)
-# jpeg("cox_plot_age_continuous.jpg")
-# ggcoxadjustedcurves(fit, data=lung)
-# dev.off()
+#creates a new col describing the overall survival for patients
+overall_survival <- as.integer( ifelse( is.na(clinic$days_to_death), clinic$days_to_last_follow_up, clinic$days_to_death) )
+clinic$overall_survival <- overall_survival
+clinic$death_event <- ifelse(clinic$patient.vital_status == 'alive', 0,1)
+colnames(clinic) #use if want to visually see affect of ^^
+cox_fit <- coxph(Surv(overall_survival, death_event)~age_at_initial_pathologic_diagnosis, data=clinic)
+jpeg("cox_plot_age_continuous.jpg")
+ggcoxadjustedcurves(fit, data=clinic)
+dev.off()
 # Use rsync to copy figure onto local system and view
 
 #######    Group 3: MAF   ###########
